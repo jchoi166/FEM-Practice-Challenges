3e5328ed6421c8a69f155be710221c2aa248308e
Spike <jchoi166@gmail.com>
2021-02-16 08:29:05 -0800

    touched up accordion functionality

--- faq-accordion-card-main/index.js
+++ faq-accordion-card-main/index.js
@@ -5,11 +5,12 @@ document.querySelectorAll('.accordion__question').forEach(button => {
         const accordionContent = button.nextElementSibling
 
         button.classList.toggle('accordion__question--active')
+        // accordionContent.classList.toggle('accordion__answer--active')
 
         if (button.classList.contains('accordion__question--active')) {
-            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+            accordionContent.style.height = (accordionContent.scrollHeight + 10) + 'px';
         } else {
-            accordionContent.style.maxHeight = 0
+            accordionContent.style.height = 0
         }
     })
 })
