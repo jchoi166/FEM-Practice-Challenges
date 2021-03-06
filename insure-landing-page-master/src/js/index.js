c8c804e644dafe6b160c72eebee20823e0a7d765
Spike <jchoi166@gmail.com>
2021-03-06 07:23:03 -0800

    sort of finished nav, need to remove mobile class when resizing to larger screen

--- insure-landing-page-master/src/js/index.js
+++ insure-landing-page-master/src/js/index.js
@@ -1,5 +1,11 @@
 import "../sass/index.scss"
 
-console.log('its working!')
+console.log('its not working?')
 
+const navLinks = document.querySelector(".nav-bar__links") 
+const navHam = document.querySelector(".nav-bar__collapsed") 
+
+navHam.addEventListener("click", () => {
+    navLinks.classList.toggle("mobile")
+})
 
