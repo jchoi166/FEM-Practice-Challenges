c7bc4dcdf61b08ed9d1cf3724d642f5273828b49
Spike <jchoi166@gmail.com>
2021-03-13 07:09:11 -0800

    finished form validation logic

--- intro-component-with-signup-form-master/index.js
+++ intro-component-with-signup-form-master/index.js
@@ -1,31 +1,48 @@
 console.log('im here')
 
-const button = document.querySelector(".form-button")
 const form = document.querySelector(".form")
-
-const fname = document.getElementById("fname")
-const fnameError = document.querySelector("#fname + span")
-
-const lname = document.getElementById("lname")
-const lnameError = document.querySelector("#lname + span")
 
 const email = document.getElementById("email")
 const emailError = document.querySelector("#email + span")
 
-const pword = document.getElementById("pword")
-const pwordError = document.querySelector("#pword + span")
+const listItems = document.querySelectorAll("ul li")
 
-const inputList = document.getElementsByTagName("input")
-
-console.log(inputList)
+console.log(listItems)
+console.log(fname)
 
 form.addEventListener("submit", () => {
     event.preventDefault()
     console.log("its working!")
 
-    // if (fname.validity.valueMissing) {
-    //     fnameError.textContent = "* Please type in your name"
-    //     // fname.value = "Please type in your name"
-    // } 
+    let canSubmit = true
+
+    for (let item of listItems) {
+        let listItem = item.children[0]
+        let errorField = item.children[1]
+        if (listItem.tagName === "BUTTON") {
+            break
+        }
+        if (listItem.validity.valueMissing) {
+            errorField.textContent += `* Please type in your ${listItem.placeholder}`
+            canSubmit = false
+        }
+    }
+
+    if (email.validity.typeMismatch) {
+        emailError.textContent = "* Please type in a valid email address!"
+        canSubmit = false
+    }
+    console.log('fuk')
+
+    if (canSubmit) {
+        window.alert('Form Submitted!')
+        for (let item of listItems) {
+            item.children[0].value = ''
+        }
+    }
     
+})
+
+form.addEventListener("input", event => {
+    event.target.nextElementSibling.innerText = ''
 })
